0aea0b03d1a980b63e9c9528795e35be0608fbe1
Jackson Abney <hickorysmokedbacon.yt@gmail.com>
2021-06-26 04:31:17 -0800

    Designed clock in/out and logon buttons

--- FFOS/DriveThruOrder.Designer.cs
+++ FFOS/DriveThruOrder.Designer.cs
@@ -30,23 +30,31 @@ namespace FFOS
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.Orders = new System.Windows.Forms.Label();
-            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
             this.Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.CheckNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Location1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Details = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Orders = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Location = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.signedInEmployeeName = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -73,6 +81,41 @@ namespace FFOS
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(625, 665);
             this.dataGridView1.TabIndex = 1;
+            // 
+            // Type
+            // 
+            this.Type.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Type.FillWeight = 15F;
+            this.Type.HeaderText = "Type";
+            this.Type.Name = "Type";
+            this.Type.ReadOnly = true;
+            this.Type.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // CheckNumber
+            // 
+            this.CheckNumber.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.CheckNumber.FillWeight = 20F;
+            this.CheckNumber.HeaderText = "Chk Num";
+            this.CheckNumber.Name = "CheckNumber";
+            this.CheckNumber.ReadOnly = true;
+            this.CheckNumber.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Location1
+            // 
+            this.Location1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Location1.FillWeight = 20F;
+            this.Location1.HeaderText = "Location";
+            this.Location1.Name = "Location1";
+            this.Location1.ReadOnly = true;
+            this.Location1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Details
+            // 
+            this.Details.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Details.HeaderText = "Details";
+            this.Details.Name = "Details";
+            this.Details.ReadOnly = true;
+            this.Details.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
             // 
             // tableLayoutPanel1
             // 
@@ -139,52 +182,6 @@ namespace FFOS
             this.dataGridView2.Size = new System.Drawing.Size(619, 665);
             this.dataGridView2.TabIndex = 1;
             // 
-            // label1
-            // 
-            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(619, 30);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Recent Swipes";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // Type
-            // 
-            this.Type.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Type.FillWeight = 15F;
-            this.Type.HeaderText = "Type";
-            this.Type.Name = "Type";
-            this.Type.ReadOnly = true;
-            this.Type.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            // 
-            // CheckNumber
-            // 
-            this.CheckNumber.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.CheckNumber.FillWeight = 20F;
-            this.CheckNumber.HeaderText = "Chk Num";
-            this.CheckNumber.Name = "CheckNumber";
-            this.CheckNumber.ReadOnly = true;
-            this.CheckNumber.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            // 
-            // Location1
-            // 
-            this.Location1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Location1.FillWeight = 20F;
-            this.Location1.HeaderText = "Location";
-            this.Location1.Name = "Location1";
-            this.Location1.ReadOnly = true;
-            this.Location1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            // 
-            // Details
-            // 
-            this.Details.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Details.HeaderText = "Details";
-            this.Details.Name = "Details";
-            this.Details.ReadOnly = true;
-            this.Details.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
-            // 
             // dataGridViewTextBoxColumn1
             // 
             this.dataGridViewTextBoxColumn1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
@@ -220,12 +217,84 @@ namespace FFOS
             this.dataGridViewTextBoxColumn3.ReadOnly = true;
             this.dataGridViewTextBoxColumn3.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
             // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(619, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Recent Swipes";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.signedInEmployeeName});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 1052);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1920, 28);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Georgia", 14F, System.Drawing.FontStyle.Bold);
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(119, 23);
+            this.toolStripStatusLabel1.Text = "Drive Thru";
+            // 
+            // signedInEmployeeName
+            // 
+            this.signedInEmployeeName.Font = new System.Drawing.Font("Georgia", 14F, System.Drawing.FontStyle.Bold);
+            this.signedInEmployeeName.Name = "signedInEmployeeName";
+            this.signedInEmployeeName.Size = new System.Drawing.Size(1755, 23);
+            this.signedInEmployeeName.Spring = true;
+            this.signedInEmployeeName.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.ForestGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(3, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(376, 115);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Sign-In";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Yellow;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(3, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(376, 115);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Clock In/Out";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(1526, 719);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(382, 330);
+            this.panel1.TabIndex = 6;
+            // 
             // DriveThruOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1920, 1080);
             this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.tableLayoutPanel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -240,7 +309,11 @@ namespace FFOS
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -259,5 +332,11 @@ namespace FFOS
         private System.Windows.Forms.DataGridViewTextBoxColumn Location;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel signedInEmployeeName;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- FFOS/Form1.cs
+++ FFOS/Form1.cs
@@ -20,25 +20,25 @@ namespace FFOS
         private async void Form1_Shown(object sender, EventArgs e)
         {
             //TO-DO: ADD ACTUAL WORK HERE
-            await Task.Delay(4000);
+            //await Task.Delay(4000);
             progressBar1.Value = 5;
             label1.Text = "Loading menu items...";
-            await Task.Delay(2500);
+            //await Task.Delay(2500);
             progressBar1.Value = 20;
             label1.Text = "Loading employee permission data...";
-            await Task.Delay(1000);
+            //await Task.Delay(1000);
             progressBar1.Value = 30;
             label1.Text = "Fetching terminal job assignment...";
-            await Task.Delay(1000);
+            //await Task.Delay(1000);
             progressBar1.Value = 40;
             label1.Text = "Loading current orders...";
-            await Task.Delay(5000);
+            //await Task.Delay(5000);
             progressBar1.Value = 70;
             label1.Text = "Checking integrity of terminal peripherals...";
-            await Task.Delay(1500);
+            //await Task.Delay(1500);
             progressBar1.Value = 90;
             label1.Text = "Preparing user interface...";
-            await Task.Delay(2000);
+            //await Task.Delay(2000);
             progressBar1.Value = 100;
             DriveThruOrder DTO = new DriveThruOrder();
             this.Hide();
